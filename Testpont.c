edbbf76c57eab04447e16a2a9d57e3c0d2523b7e
Carlos98770 <eduardoabc.eduardo9@gmail.com>
2024-03-07 14:27:32 -0300

    Praticando

--- Testpont.c
+++ Testpont.c
@@ -1,13 +1,58 @@
 #include <stdio.h>
+/*
+void multiplica_matrizes(int *pA,int *pB,int *pC, int x, int y, int z){
+    //B[y][z]
+    //A[x][y]
 
-int main(void){
-    int m = 2;
-    int A[m][m] = {{1,2},{2,3}};
 
+
+
+
+
+
+}
+*/
+
+
+void prencherMatriz(int *pa, int m){
     for(int i = 0; i < m; i++){
-        for(int j = 0; j < m; j++)
-            printf("%d ", A[i][j]);
+        for(int j = 0; j < m; j++){
+            scanf("%d", pa);
+            pa++;
+        }
             
     }
+}
+
+
+void imprimirMatriz(int *pa, int m){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d ", *pa);
+            pa++;
+        }
+
+        printf("\n");
+    }
+
+}
+
+
+int main(void){
+    int m; 
+    scanf("%d" , &m);
+    int A[m][m];
+    int *pa;
+    pa = A[0];
+
+    prencherMatriz(pa,m);
+    a(pa,m);
+    //imprimirMatriz(pa,m);
+    
+
+    
+
+
+
     return 0;
 }
