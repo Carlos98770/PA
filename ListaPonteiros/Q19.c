76e2ee2104b7f0ef60d4617ed4c312f6be594325
Carlos98770 <eduardoabc.eduardo9@gmail.com>
2024-03-15 14:34:13 -0300

    Fazendo Q19

--- ListaPonteiros/Q19.c
+++ ListaPonteiros/Q19.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void soma_vetores(int vet1[], int vet2[], int resultado[],int n){
+void soma_vetores(int *vet1, int *vet2, int *resultado,int n){
     for(int i = 0; i < n; i++)
         resultado[i] = vet1[i] + vet2[i];
     
 }
+void prencherVetor(int *vet, size_t n ){
+    
+    for(int i = 0; i < n; i++){
+        int p;
+        scanf("%d" , &p);
+        vet[i] = p;
+    
+    }
 
+}
+void imprimirVetor( int *vet, size_t n){
+    for(int i = 0; i < n; i++ )
+        printf("%d " , vet[i]);
+}
 
 
 int main(){
-    int n;
+
     printf("digite o tamanho do vetor: \n");
-    scanf("%d", &n);
-    int vet1 [n];
-    int vet2 [n];
-    int resultado [n];
-    for(int i = 0; i < n; i++)
-        scanf("%d" , &vet1[i]);
+    size_t n;
+    scanf("%zu", &n);
+    int *vet1;
+    int *vet2;
+    int *resultado;
     
-    for(int i = 0; i < n; i++)
-        scanf("%d" , &vet2[i]);
+    vet1 = (int *) malloc( n * sizeof(int));
+    vet2 = (int *)malloc( n * sizeof(int));
+    resultado = (int *)malloc( n * sizeof(int));
 
-
+    printf("Elemento de v1: \n");
+    prencherVetor(vet1 , n);
+    printf("Elemento de v2: \n");
+    prencherVetor(vet2 , n);
     
     soma_vetores(vet1, vet2, resultado, n);
-    printf("Resultado da Soma Vetorial: ");
-    for(int i = 0; i < n; i++)
-        printf("%d ", resultado[i]);
+    printf("Elemento do vetor resultante: ");
+    imprimirVetor(resultado,n);
+    
+    free(vet1);
+    free(vet2);
+    
 
     
     return 0;
